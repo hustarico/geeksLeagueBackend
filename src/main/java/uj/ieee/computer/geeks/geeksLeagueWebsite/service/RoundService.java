5002d06171b457924f4f2d8e6c8dc66d325afb54
hussam tarteer <hussamtarteer74@gmail.com>
2025-05-17 17:30:41 +0300

    random fixes all over the place

--- src/main/java/uj/ieee/computer/geeks/geeksLeagueWebsite/service/RoundService.java
+++ src/main/java/uj/ieee/computer/geeks/geeksLeagueWebsite/service/RoundService.java
@@ -2,14 +2,18 @@ package uj.ieee.computer.geeks.geeksLeagueWebsite.service;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-
+import uj.ieee.computer.geeks.geeksLeagueWebsite.dto.RoundDTO;
 import uj.ieee.computer.geeks.geeksLeagueWebsite.entity.Round;
+import uj.ieee.computer.geeks.geeksLeagueWebsite.enums.RoundStatus;
 import uj.ieee.computer.geeks.geeksLeagueWebsite.exceptions.RoundNotFoundException;
 import uj.ieee.computer.geeks.geeksLeagueWebsite.repo.RoundRepo;
+
+
 
 @Service
 public class RoundService{
@@ -18,17 +22,44 @@ public class RoundService{
     public RoundService(RoundRepo repo) {
         this.repo = repo;
     }
+
+    public Round findByIdRaw(long id){
+        return repo.findById(id).orElseThrow(()->new RoundNotFoundException("Round \"%d\" not found".formatted(id)));
+    }
     
-    public Round findById(long id){
-        return repo.findById(id).orElseThrow(()->new RoundNotFoundException(id));
+    private RoundStatus getStatus(Round round){
+
+        var now = LocalDate.now();
+        if(round.getEndingDate().isBefore(now))return RoundStatus.FINISHED;
+        if(round.getStartingDate().isBefore(now) && round.getEndingDate().isAfter(now))return RoundStatus.ACTIVE;
+        else
+        return RoundStatus.UPCOMING;
+
+        
+    }
+
+    public RoundDTO findById(long id){
+        var round = repo.findById(id).get();
+
+        if(round == null)throw new RoundNotFoundException("Round \"%d\" not found".formatted(id));
+
+        else
+        return new RoundDTO(round.getId(), round.getTitle(), round.getStartingDate(), round.getEndingDate(), round.getDescription(),getStatus(round));
     }
 
     public Round findByTitle(String title){
         return repo.findByTitle(title).orElseThrow(()->new RoundNotFoundException("Round \"%s\" not found".formatted(title)));
     }
 
-    public List<Round> findAll(){
-        return repo.findAll();
+    public List<RoundDTO> findAll(){
+
+        var allRounds=  repo.findAll();
+        
+        var allRoundsDto = new ArrayList<RoundDTO>() ;
+        for(Round round :allRounds){
+            allRoundsDto.add(new RoundDTO(round.getId(), round.getTitle(), round.getStartingDate(), round.getEndingDate(), round.getDescription(),getStatus(round)));
+        }
+        return allRoundsDto;
     }
 
     public Round findActiveRound(){ 
@@ -46,7 +77,7 @@ public class RoundService{
     }
 
     public Round update(long id, Round roundDetails) {
-        Round round = findById(id);
+        Round round = findByIdRaw(id);
         round.setTitle(roundDetails.getTitle());
         round.setStartingDate(roundDetails.getStartingDate());
         round.setEndingDate(roundDetails.getEndingDate());
@@ -55,7 +86,7 @@ public class RoundService{
     }
 
     public void delete(long id) {
-        Round round = findById(id);
+        Round round = findByIdRaw(id);
         repo.delete(round);
     }
 
